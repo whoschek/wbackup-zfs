d23c48e13c0221a195c89db499bc55efd4529d82
Wolfgang Hoschek <wolfgang.hoschek@mac.com>
2024-08-07 12:13:49 +0200

    move away from csh

--- .github-workflow-scripts/test_freebsd_13_3.sh
+++ .github-workflow-scripts/test_freebsd_13_3.sh
@@ -14,11 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Note: csh syntax following here as default shell on freebsd <= 13 is csh instead of sh
+set -e # Exit immediately if a cmd returns a non-zero status
 pkg install -y python3 sudo zstd pv mbuffer devel/py-coverage
 id -u -n
 uname -a
-zfs --version
+zfs --version || true
 python3 --version
 ssh -V
 zstd --version
@@ -31,27 +31,25 @@ mkdir -p $HOME/.ssh
 rm -f $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa.pub
 ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -q -N ""  # create private key and public key
 cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
-ls -al $HOME
-chsh -s /bin/sh
+ls -al $HOME $HOME/.ssh/id_rsa
 
-echo "Now running tests as root user"; ./test.sh; if ($status != 0) exit 1
-echo "Now running coverage"; ./coverage.sh; if ($status != 0) exit 1
+chsh -s /bin/sh
+echo "Now running tests as root user"; ./test.sh
+echo "Now running coverage"; ./coverage.sh
 
 echo "Now running tests as non-root user:"
-set tuser = test
-set thome = /home/$tuser
-#pw userdel -n $tuser
+tuser=test
+thome=/home/$tuser
+#pw userdel -n $tuser || true
 pw useradd $tuser -d $thome -m
-echo "$tuser ALL=NOPASSWD:`command -v zfs`,`command -v zpool`" >> /usr/local/etc/sudoers
+echo "$tuser ALL=NOPASSWD:$(command -v zfs),$(command -v zpool)" >> /usr/local/etc/sudoers
 
 mkdir -p $thome/.ssh
 cp -p $HOME/.ssh/id_rsa $HOME/.ssh/id_rsa.pub $HOME/.ssh/authorized_keys $thome/.ssh/
-chmod go-rwx $thome/.ssh/authorized_keys
-chown -R $tuser $thome/.ssh
+chmod go-rwx "$thome/.ssh/authorized_keys"
+chown -R "$tuser" "$thome/.ssh"
 
-cp -Rp . $thome/wbackup-zfs # if running with Github Action
-# cp -Rp wbackup-zfs $thome/ # if running with https://github.com/vmactions/shell-freebsd
-chown -R $tuser $thome/wbackup-zfs
+cp -R . "$thome/wbackup-zfs"
+chown -R "$tuser" "$thome/wbackup-zfs"
 chsh -s /bin/sh $tuser
-sudo -u $tuser sh -c "cd $thome/wbackup-zfs; ./test.sh"; if ($status != 0) exit 1
-echo "wbackup-zfs-testrun-success"
+sudo -u $tuser sh -c "cd $thome/wbackup-zfs; ./test.sh"
